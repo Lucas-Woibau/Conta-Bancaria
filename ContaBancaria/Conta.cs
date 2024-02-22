5336d2a93b6d6c8f5dd5a9fb94453597d7754ec2
Lucas Woibau <lucaswoibau7@gmail.com>
2024-02-22 16:55:05 -0300

    Add project files.

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+        private int NumeroConta { get; set; }
+        private string _nome;
+        public double ValorInicial { get; set; }
+        public double Saldo { get; set; }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if(value.Length > 1 && value != null)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + 
+                NumeroConta.ToString() + ", " + 
+                "Titular: " + 
+                Nome + ", " + 
+                "Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture) + "R$.";
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
